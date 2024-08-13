c2249367100e1e111d6106991cfd4b6f2b451131
hamidh-2k8 <hamidh.2k8@gmail.com>
2024-08-13 18:49:30 -0400

    Added OTOS code (expand) - Added OpticalSensor.java as a wrapper for the OTOS code - Added AngleMath.java from Gobo - Added OTOS Telemetry Output to FishTesting.java - Added Virtual Fence Drive Method to Drivetrain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/FishTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/FishTesting.java
@@ -1,22 +1,28 @@
  package org.firstinspires.ftc.teamcode.teleops;
 
+ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
  import org.firstinspires.ftc.teamcode.OD.FishDetector;
  import com.qualcomm.robotcore.eventloop.opmode.OpMode;
  import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
- import org.firstinspires.ftc.teamcode.math_utils.Point;
+
+ import org.firstinspires.ftc.teamcode.odom.OpticalSensor;
 
  @TeleOp(name="FISH TESTING", group = "Howard")
  public class FishTesting extends OpMode
  {
 
     FishDetector fd;
+    OpticalSensor od;
 
      /*
       * Code to run ONCE when the driver hits INIT
       */
      @Override
      public void init() {
-        fd  = new FishDetector(hardwareMap);
+
+         fd  = new FishDetector(hardwareMap);
+         od = new OpticalSensor("otos", hardwareMap, DistanceUnit.METER, AngleUnit.RADIANS);
      }
   
      /*
@@ -25,8 +31,11 @@
      @Override
      public void loop() {
          fd.update();
+         od.update();
          telemetry.addData("FISH", fd.getFishCoords());
          telemetry.addData("Calculated", fd.getXYInput());
+         telemetry.addData("Odometry Coords", od.getPosition().x + "," + od.getPosition().y);
+         telemetry.addData("Odometry Angle", od.getPosition().angle());
      }
  
      /*
@@ -34,6 +43,9 @@
       */
      @Override
      public void stop() {
+
          fd.quit();
+         od.quit();
+
      }
  }
